77520c5c5b54cdf0c5c29f39fae9e04f8f29e836
RukaiyaMustafizu <rukaiyamustafizu@icloud.com>
2025-02-21 01:23:31 +0100

    change html,css,java and added some new features
    This Ai generator allow users to generate poems in languages they wish.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 function displayPoem(response) {
   console.log("poem generated");
+  let poemElement = document.querySelector("#poem");
+  let emojiElement = document.querySelector("#loading-emoji");
+  if (emojiElement) {
+    emojiElement.classList.remove("rotating-emoji");
+  }
   new Typewriter("#poem", {
     strings: response.data.answer,
     loop: false,
@@ -9,17 +14,29 @@ function displayPoem(response) {
   });
 }
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
 function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "tf315a00255a026o44c386706557b731";
-  let prompt = `User instructions are: Generate a Dutch poem about love ${instructionsInput.value}`;
+  let prompt = `User instructions are: Generate any language poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poet who is inspired by the beauty of nature. Your mission is to generate a 4-line poem about love in basic HTML avoid displaying  html. Make sure to follow the instructions given by the user. Sign he poem wih RUKY";
+    "You are a romantic poet who is inspired by the beauty of nature. Your mission is to generate a 4-line poem about anything inspiring in basic HTML avoid displaying html. Make sure to follow the instructions given by the user. Sign the poem with <div> <strong> 🩷RUKY </strong></div> in pink and keep it below the poem on a new line. If a language is typed wrongly, auto correct it and also please try to listen to the user's command";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("Generating poem");
   console.log(`prompt: ${prompt}`);
   console.log(`context: ${context}`);
+  let poemElement = document.querySelector("#poem");
+  let emojiElement = document.createElement("span");
+  emojiElement.id = "loading-emoji";
+  emojiElement.classList.add("rotating-emoji");
+  emojiElement.textContent = "⏳";
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating poem...about ${instructionsInput.value}`;
+  poemElement.prepend(emojiElement);
   axios.get(apiURL).then(displayPoem);
 }
 
