d8ed88a197bcf60661e8751c8445e0b95011889b
RukaiyaMustafizu <rukaiyamustafizu@icloud.com>
2025-02-21 01:29:53 +0100

    removed console.logs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
   let poemElement = document.querySelector("#poem");
   let emojiElement = document.querySelector("#loading-emoji");
   if (emojiElement) {
@@ -22,20 +21,20 @@ function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "tf315a00255a026o44c386706557b731";
-  let prompt = `User instructions are: Generate any language poem about ${instructionsInput.value}`;
+  let inputText = instructionsInput.value.trim();
+  let language = capitalizeFirstLetter(inputText.split(" ")[0]);
+  let topic = inputText.split(" ").slice(1).join(" ");
+  let prompt = `Generate a poem in ${language} about ${topic}`;
   let context =
     "You are a romantic poet who is inspired by the beauty of nature. Your mission is to generate a 4-line poem about anything inspiring in basic HTML avoid displaying html. Make sure to follow the instructions given by the user. Sign the poem with <div> <strong> 🩷RUKY </strong></div> in pink and keep it below the poem on a new line. If a language is typed wrongly, auto correct it and also please try to listen to the user's command";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("Generating poem");
-  console.log(`prompt: ${prompt}`);
-  console.log(`context: ${context}`);
   let poemElement = document.querySelector("#poem");
   let emojiElement = document.createElement("span");
   emojiElement.id = "loading-emoji";
   emojiElement.classList.add("rotating-emoji");
   emojiElement.textContent = "⏳";
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `Generating poem...about ${instructionsInput.value}`;
+  poemElement.innerHTML = `Generating poem...about ${topic}`;
   poemElement.prepend(emojiElement);
   axios.get(apiURL).then(displayPoem);
 }
